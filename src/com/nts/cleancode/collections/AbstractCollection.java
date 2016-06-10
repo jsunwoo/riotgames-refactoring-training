c577c5e17b866323c96a3ad18c40b20972d6d3fb
ChoJunYoung <jjy891224@gmail.com>
2016-06-10 16:56:12 +0900

    Modify add

--- src/com/nts/cleancode/collections/AbstractCollection.java
+++ src/com/nts/cleancode/collections/AbstractCollection.java
@@ -1,93 +1,91 @@
 package com.nts.cleancode.collections;
 
 public abstract class AbstractCollection {
-   protected static int INITIAL_CAPACITY = 10;
-   protected Object[] elements = new Object[INITIAL_CAPACITY];
-   protected boolean readOnly;
-   protected int size = 0;
+    protected static int INITIAL_CAPACITY = 10;
+    protected Object[] elements = new Object[INITIAL_CAPACITY];
+    protected boolean readOnly;
+    protected int size = 0;
 
-   public void addAll(AbstractCollection collection) {
-      for (int i=0; i < collection.size(); i++) {
-         if (!contains(collection.get(i))) {
-            add(collection.get(i));
-         }
-      }
-   }
-   
-   public void add(Object key, Object value) {
-   }
+    public void addAll(AbstractCollection collection) {
+        for (int i = 0; i < collection.size(); i++) {
+            if (!contains(collection.get(i))) {
+                add(collection.get(i));
+            }
+        }
+    }
 
-   public boolean isEmpty() {
-      return size == 0;
-   }
 
-   public void add(Object element) {
-      if (readOnly)
-         return;
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-         if (shouldgorw())
+    public void add(Object element) {
+        if (readOnly)
+            return;
+
+        if (shouldgorw())
             grow();
 
-         addElement(element);
+        addElement(element);
 
-   }
+    }
 
-   public boolean remove(Object element) {
-      if (readOnly)
-         return false;
-      else
-         for (int i = 0; i < size; i++)
-            if (elements[i].equals(element)) {
-               elements[i] = null;
-               Object[] newElements = new Object[size - 1];
-               int k = 0;
-               for (int j = 0; j < size; j++) {
-                  if (elements[j] != null)
-                     newElements[k++] = elements[j];
-               }
-               size--;
-               elements = newElements;
-               return true;
-            }
-      return false;
-   }
+    public boolean remove(Object element) {
+        if (readOnly)
+            return false;
+        else
+            for (int i = 0; i < size; i++)
+                if (elements[i].equals(element)) {
+                    elements[i] = null;
+                    Object[] newElements = new Object[size - 1];
+                    int k = 0;
+                    for (int j = 0; j < size; j++) {
+                        if (elements[j] != null)
+                            newElements[k++] = elements[j];
+                    }
+                    size--;
+                    elements = newElements;
+                    return true;
+                }
+        return false;
+    }
 
-   protected boolean shouldgorw() {
-      return size + 1 > capacity();
-   }
+    protected boolean shouldgorw() {
+        return size + 1 > capacity();
+    }
 
-   protected Object addElement(Object element) {
-      return elements[size++] = element;
-   }
+    protected Object addElement(Object element) {
+        return elements[size++] = element;
+    }
 
-   protected void grow() {
-      Object[] newElements =
-         new Object[capacity() + 10];
-      for (int i = 0; i < size; i++)
-         newElements[i] = elements[i];
-      elements = newElements;
-   }
+    protected void grow() {
+        Object[] newElements =
+                new Object[capacity() + 10];
+        for (int i = 0; i < size; i++)
+            newElements[i] = elements[i];
+        elements = newElements;
+    }
 
-   public boolean contains(Object element) {
-      for (int i=0; i<size; i++)
-         if (elements[i].equals(element))
-            return true;
-      return false;
-   }
+    public boolean contains(Object element) {
+        for (int i = 0; i < size; i++)
+            if (elements[i].equals(element))
+                return true;
+        return false;
+    }
 
-   public int size() {
-      return size;
-   }
+    public int size() {
+        return size;
+    }
 
-   public Object get(int index) {
-      return elements[index];
-   }
+    public Object get(int index) {
+        return elements[index];
+    }
 
-   public void setReadOnly(boolean b) {
-      readOnly = b;
-   }
+    public void setReadOnly(boolean b) {
+        readOnly = b;
+    }
 
-   public int capacity() {
-      return elements.length;
-   }
+    public int capacity() {
+        return elements.length;
+    }
 }
