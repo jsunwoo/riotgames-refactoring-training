d89620f61e26962aa5c2a25ecb95209877472c52
ChoJunYoung <jjy891224@gmail.com>
2016-06-10 16:53:30 +0900

    Pullup add and another

--- src/com/nts/cleancode/collections/AbstractCollection.java
+++ src/com/nts/cleancode/collections/AbstractCollection.java
@@ -3,6 +3,9 @@ package com.nts.cleancode.collections;
 public abstract class AbstractCollection {
    protected static int INITIAL_CAPACITY = 10;
    protected Object[] elements = new Object[INITIAL_CAPACITY];
+   protected boolean readOnly;
+   protected int size = 0;
+
    public void addAll(AbstractCollection collection) {
       for (int i=0; i < collection.size(); i++) {
          if (!contains(collection.get(i))) {
@@ -14,17 +17,59 @@ public abstract class AbstractCollection {
    public void add(Object key, Object value) {
    }
 
-   public abstract boolean isEmpty();
+   public boolean isEmpty() {
+      return size == 0;
+   }
 
-   public abstract void add(Object element);
+   public void add(Object element) {
+      if (readOnly)
+         return;
+
+         if (shouldgorw())
+            grow();
+
+         addElement(element);
+
+   }
 
    public abstract boolean remove(Object element);
 
-   public abstract boolean contains(Object element);
+   protected boolean shouldgorw() {
+      return size + 1 > capacity();
+   }
 
-   public abstract int size();
+   protected Object addElement(Object element) {
+      return elements[size++] = element;
+   }
+
+   protected void grow() {
+      Object[] newElements =
+         new Object[capacity() + 10];
+      for (int i = 0; i < size; i++)
+         newElements[i] = elements[i];
+      elements = newElements;
+   }
+
+   public boolean contains(Object element) {
+      for (int i=0; i<size; i++)
+         if (elements[i].equals(element))
+            return true;
+      return false;
+   }
+
+   public int size() {
+      return size;
+   }
 
    public Object get(int index) {
       return elements[index];
+   }
+
+   public void setReadOnly(boolean b) {
+      readOnly = b;
+   }
+
+   public int capacity() {
+      return elements.length;
    }
 }
--- src/com/nts/cleancode/collections/List.java
+++ src/com/nts/cleancode/collections/List.java
@@ -1,51 +1,6 @@
 package com.nts.cleancode.collections;
 
 public class List extends AbstractCollection{
-   
-   private int size = 0;
-   private boolean readOnly;
-
-   public boolean isEmpty() {
-      return size == 0;
-   }
-
-   public void add(Object element) {
-      if (readOnly)
-         return;
-          
-         if (shouldgorw()) 
-            grow();
-         
-         addElement(element);
-      
-   }
-
-   protected boolean shouldgorw() {
-      return size + 1 > elements.length;
-   }
-
-   protected Object addElement(Object element) {
-      return elements[size++] = element;
-   }
-
-   protected void grow() {
-      Object[] newElements =
-         new Object[elements.length + 10];
-      for (int i = 0; i < size; i++)
-         newElements[i] = elements[i];
-      elements = newElements;
-   }
-
-   public boolean contains(Object element) {
-      for (int i=0; i<size; i++) 
-         if (elements[i].equals(element))
-            return true;
-      return false;
-   }
-
-   public int size() {
-      return size;
-   }
 
    public boolean remove(Object element) {
       if (readOnly)
@@ -66,10 +21,6 @@ public class List extends AbstractCollection{
             }
       return false;
    }
-   
-   public int capacity() {
-      return elements.length;
-   }
 
    public void set(int i, Object value) {
       if (!readOnly) {
@@ -79,7 +30,4 @@ public class List extends AbstractCollection{
       }
    }
 
-   public void setReadOnly(boolean b) {
-      readOnly = b;
-   }
 }
