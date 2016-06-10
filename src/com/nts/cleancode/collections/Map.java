b7c1226e5ed1b8ebec5c2ae04d3dafbfc0df0b27
jsunwoo <jsunwoo@ualberta.ca>
2016-06-10 18:32:38 +0900

    Delete add in Map Class

--- src/com/nts/cleancode/collections/Map.java
+++ src/com/nts/cleancode/collections/Map.java
@@ -1,107 +1,102 @@
 package com.nts.cleancode.collections;
 
 public class Map {
-    private static int INITIAL_CAPACITY = 10;
-    protected Object[] keys = new Object[INITIAL_CAPACITY];
-    protected Object[] values = new Object[INITIAL_CAPACITY];
-    private int size = 0;
-    private int indexWhereKeyFound;
-    private boolean readOnly;
+	private static int INITIAL_CAPACITY = 10;
+	protected Object[] keys = new Object[INITIAL_CAPACITY];
+	protected Object[] values = new Object[INITIAL_CAPACITY];
+	private int size = 0;
+	private int indexWhereKeyFound;
+	private boolean readOnly;
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
-    // Do nothing because user must input key and value
-    public void add(Object element) {
-    }
+	public void addAll(Map m) {
+		for (int i = 0; i < m.size(); i++)
+			add(m.keys[i], m.values[i]);
+	}
 
-    public void addAll(Map m) {
-        for (int i = 0; i < m.size(); i++)
-            add(m.keys[i], m.values[i]);
-    }
+	public void add(Object key, Object value) {
+		if (readOnly)
+			return;
+		for (int i = 0; i < size; i++)
+			if (keys[i].equals(key)) {
+				values[i] = value;
+				return;
+			}
 
-    public void add(Object key, Object value) {
-        if (readOnly)
-            return;
-        for (int i = 0; i < size; i++)
-            if (keys[i].equals(key)) {
-                values[i] = value;
-                return;
-            }
+		if (shouldgorw()) {
+			grow();
+		}
 
-        if (shouldgorw()) {
-            grow();
-        }
+		addElement(key, value);
 
-        addElement(key, value);
+	}
 
-    }
+	private void addElement(Object key, Object value) {
+		keys[size] = key;
+		values[size] = value;
+		size++;
+	}
 
-    private void addElement(Object key, Object value) {
-        keys[size] = key;
-        values[size] = value;
-        size++;
-    }
+	private void grow() {
+		Object[] newKeys = new Object[keys.length + INITIAL_CAPACITY];
+		Object[] newValues = new Object[keys.length + INITIAL_CAPACITY];
+		System.arraycopy(keys, 0, newKeys, 0, size);
+		System.arraycopy(values, 0, newValues, 0, size);
+		keys = newKeys;
+		values = newValues;
+	}
 
-    private void grow() {
-        Object[] newKeys = new Object[keys.length + INITIAL_CAPACITY];
-        Object[] newValues = new Object[keys.length + INITIAL_CAPACITY];
-        System.arraycopy(keys, 0, newKeys, 0, size);
-        System.arraycopy(values, 0, newValues, 0, size);
-        keys = newKeys;
-        values = newValues;
-    }
+	private boolean shouldgorw() {
+		return size + 1 > keys.length;
+	}
 
-    private boolean shouldgorw() {
-        return size + 1 > keys.length;
-    }
+	public int size() {
+		return size;
+	}
 
-    public int size() {
-        return size;
-    }
+	public boolean remove(Object key) {
+		if (readOnly)
+			return false;
+		for (int i = 0; i < size; i++)
+			if (keys[i].equals(key)) {
+				keys[i] = null;
+				values[i] = null;
+				size--;
+				return true;
+			}
+		return false;
+	}
 
-    public boolean remove(Object key) {
-        if (readOnly)
-            return false;
-        for (int i = 0; i < size; i++)
-            if (keys[i].equals(key)) {
-                keys[i] = null;
-                values[i] = null;
-                size--;
-                return true;
-            }
-        return false;
-    }
+	public boolean contains(Object value) {
+		for (int i = 0; i < size; i++)
+			if ((value == null && values[i] == null) || (values[i] != null && values[i].equals(value)))
+				return true;
+		return false;
+	}
 
-    public boolean contains(Object value) {
-        for (int i = 0; i < size; i++)
-            if ((value == null && values[i] == null)
-                    || (values[i] != null && values[i].equals(value)))
-                return true;
-        return false;
-    }
+	public boolean containsKey(Object key) {
+		for (int i = 0; i < size; i++)
+			if (keys[i] != null && keys[i].equals(key)) {
+				indexWhereKeyFound = i;
+				return true;
+			}
+		return false;
+	}
 
-    public boolean containsKey(Object key) {
-        for (int i = 0; i < size; i++)
-            if (keys[i] != null && keys[i].equals(key)) {
-                indexWhereKeyFound = i;
-                return true;
-            }
-        return false;
-    }
+	public Object get(Object key) {
+		if (!containsKey(key))
+			return null;
+		return values[indexWhereKeyFound];
+	}
 
-    public Object get(Object key) {
-        if (!containsKey(key))
-            return null;
-        return values[indexWhereKeyFound];
-    }
+	public int capacity() {
+		return keys.length;
+	}
 
-    public int capacity() {
-        return keys.length;
-    }
-
-    public void setReadOnly(boolean b) {
-        readOnly = b;
-    }
+	public void setReadOnly(boolean b) {
+		readOnly = b;
+	}
 }
