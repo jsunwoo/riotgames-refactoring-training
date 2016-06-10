d6532b8b5af8a3c663c3af4dd4c5fd43eb61fb4d
ChoJunYoung <jjy891224@gmail.com>
2016-06-10 16:56:24 +0900

    Modify add

--- src/com/nts/cleancode/collections/List.java
+++ src/com/nts/cleancode/collections/List.java
@@ -1,13 +1,13 @@
 package com.nts.cleancode.collections;
 
-public class List extends AbstractCollection{
+public class List extends AbstractCollection {
 
-   public void set(int i, Object value) {
-      if (!readOnly) {
-         if (i >= size)
-            throw new ArrayIndexOutOfBoundsException();
-         elements[i] = value;
-      }
-   }
+    public void set(int i, Object value) {
+        if (!readOnly) {
+            if (i >= size)
+                throw new ArrayIndexOutOfBoundsException();
+            elements[i] = value;
+        }
+    }
 
 }
